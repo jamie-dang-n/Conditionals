49df6561f9938b5934daf543b0d3b114a65fecb9
Anika <pikainka@gmail.com>
2019-07-09 13:04:46 -0700

    First Commit

---
+++ Contents.swift
@@ -0,0 +1,108 @@
+import UIKit
+
+//var num1 = 1
+//var num2 = 2
+//var num3 = 3
+//var num5 = 5
+//var num6 = 6
+//var num7 = 7
+//var num8 = 8
+
+//true or false
+
+//this prints true
+//print(num1<num2)
+
+//this prints false
+//print (num1>=num2)
+
+//this prints false
+//print(num1==num2)
+
+//checking if num1 is not equal to num2, is true
+//print(num1 != num2)
+//if using !, space it out in swift
+
+//check if 1+2 == 3
+//if num1+num2 == num3 && num2+num1 == num3{
+//    //then do sumn babey!!!
+//    print("yes!")
+//}
+
+//check if 1 = 2
+//if num1 == num2 || num1 == num1{
+//    print("yes!")
+//}
+
+//check if 5 < 3
+//if num5<num3 {
+//    print("yes!")
+//}
+
+//simply print out true/false of 5<3. it is false
+//print(5<3)
+
+//print("Karlie" == "karlie")
+
+//why does this have an error?
+//print(7 == "7")
+
+//var dogAge = 18
+//
+////if this is true,
+//if dogAge > 10 {
+//    //then I do this
+//    print("Old doggie!")
+//}
+
+//var dogAge = 1
+//
+////if this is true
+//if dogAge > 10 { //older than 10
+//    //then do this
+//    print ("Old doggie")
+//} else if dogAge == 10{ //exactly 10
+//    //then I do this instead
+//    print ("Young puppy")
+//} else { //otherwise
+//    print ("Young puppy")
+//}
+
+//if this is true,
+//if dogAge < 2 {
+//    //then I do this
+//    print("You are baby")
+//}
+
+//if dogAge == 2 {
+//    print("You are awesome!")
+//}
+//
+//if dogAge == 3 {
+//    print("You are awesome!")
+//}
+//
+//if dogAge == 4 {
+//    print("You are awesome!")
+//}
+//
+////etc
+//
+//if dogAge > 12 {
+//    print("You are elderly")
+//}
+
+
+var cravedFood = "Boba"
+var food1 = "Chipotle"
+var food2 = "Starbucks"
+
+if cravedFood == food1 {
+    print("Aw yeah! I'm hongry")
+} else if food2 == food1 {
+    print("I still hunger")
+} else if cravedFood == "Raisinets" {
+    print("Elora wants. She desires.")
+}else {
+    print("...... Cthulhu hungers...")
+}
